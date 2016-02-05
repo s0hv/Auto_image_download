22c12483ac05ff128e0c3b15a7b3da4817357526
s0hvaperuna <tariqharb54@gmail.com>
2016-02-05 20:41:23 +0200

    Now works with direct_links

--- file_download.py
+++ file_download.py
@@ -4,28 +4,31 @@ from selenium.common.exceptions import NoSuchElementException
 from imgur import Imgur
 from gfycat import Gfycat
 from deviantart import Deviantart
-import logging, os, time
+from direct_links import DirectLink
+import os, time
 
 
 class GetFiles(object):
-    # Set up the variables
-    client_id = None
-    client_secret = None
-    path = ""
-    # Image sites
-    gfycat = Gfycat()
-    imgur = None
-    deviantart = None
-    # The logger
-    logging.basicConfig(level=logging.WARNING, filename="Errors/Error.log", format='%(asctime)s')
-    # Chrome status
-    chrome_working = False
-    chrome_on = False
-    chrome_extension = ""
+
+    def __init__(s):
+        # Set up the variables
+        s.path = ""
+        s.phantom_on = False
+        s.phantom_driver = None
+        s.phantom_path = ""
+        # Image sites
+        s.gfycat = Gfycat()
+        s.imgur = None
+        s.deviantart = None
+        # Chrome status
+        s.driver = None
+        s.chrome_working = False
+        s.chrome_on = False
+        s.chrome_extension = ""
 
     # Checks files in path and reads them. If image download fails the text file will not be deleted.
-    def get_images(self):
-        global imgur, gfycat, deviantart, path, chrome_extension
+    def get_images(s):
+        path = s.path
         files = [i for i in os.listdir(path) if ".txt" in i]
         for file in files:
             opened = path + "\\" + file
@@ -35,7 +38,7 @@ class GetFiles(object):
                 # Imgur images
                 if 'imgur.com/' in link:
                     print(link)
-                    result = imgur.get(link)
+                    result = s.imgur.get(link)
                     print("Result: ", result, opened)
                     if result is not True:
                         os.rename(opened, path + "\\Imgur\\Big albums\\" + result + ".txt")
@@ -46,7 +49,7 @@ class GetFiles(object):
                 # Gfycat images
                 elif 'gfycat.com' in link:
                     gfy_id = link.split("/")[-1].split(".")[0]
-                    result = gfycat.get_image(gfy_id)
+                    result = s.gfycat.get_image(gfy_id)
                     print(result)
                     if result and f.closed:
                         os.remove(opened)
@@ -55,18 +58,24 @@ class GetFiles(object):
                         print("Something went wrong in gfycat")
                 # DeviantArt images. Doesn't work if Chrome didn't start correctly
                 elif 'deviantart.com' in link:
-                    if not chrome_on:
-                        self.open_chrome(chrome_extension)
-                    if chrome_working:
-                        self.close_windows(driver.current_url)
-                        result = deviantart.get_image(opened, link)
+                    if not s.chrome_on:
+                        s.open_chrome(s.chrome_extension)
+                    if s.chrome_working:
+                        s.close_windows(s.driver.current_url)
+                        result = s.deviantart.get_image(opened, link)
                         if result:
                             print("Nice", result)
                         else:
                             print("Fuck")
+                else:
+                    if not s.phantom_on:
+                        s.start_phantomjs()
+                    result = DirectLink.download_image(link, opened, s.phantom_driver)
+                    if not result:
+                        print("Fuck")
 
-    @staticmethod
-    def close_windows(curr_url):
+    def close_windows(s, curr_url):
+        driver = s.driver
         h = driver.window_handles
         saved_handle = h[0]
         for n in h:
@@ -78,15 +87,14 @@ class GetFiles(object):
                 saved_handle = n
         driver.switch_to.window(saved_handle)
 
-    @staticmethod
-    def open_chrome(extension_path):
-        global chrome_on, chrome_working, driver, deviantart
+    def open_chrome(s, extension_path):
         # Initialize Chrome
         options = webdriver.ChromeOptions()
         options.add_extension(extension_path)
-        driver = webdriver.Chrome(chrome_options=options)
+        s.driver = webdriver.Chrome(chrome_options=options)
+        driver = s.driver
         driver.set_window_position(x=-2000, y=0)
-        chrome_on = True
+        s.chrome_on = True
         # Install Userscripts to Tampermonkey
         driver.get('chrome://extensions/dhdgffkkebhmkfjojejmpbldmpobfkfod')
         time.sleep(1)
@@ -108,32 +116,31 @@ class GetFiles(object):
                 try:
                     elem = driver.find_element_by_id('input_dG1fSW5zdGFsbA')
                     elem.click()
-                    chrome_working = True
-                    deviantart = Deviantart(driver)
+                    s.chrome_working = True
+                    s.deviantart = Deviantart(driver)
                 except NoSuchElementException:
-                    chrome_working = False
+                    s.chrome_working = False
 
     # Imgur api auth
-    @staticmethod
-    def set_imgur(id, secret):
-        global client_id, client_secret, imgur
-        client_secret = secret
-        client_id = id
-        client = ImgurClient(client_id, client_secret)
-        imgur = Imgur(imgurclient=client)
+    def set_imgur(s, id, secret):
+        client = ImgurClient(id, secret)
+        s.imgur = Imgur(imgurclient=client)
 
     # Set path to image folder
-    @staticmethod
-    def set_path(image_path):
-        global path
-        path = image_path
+    def set_path(s, image_path):
+        s.path = image_path
 
-    @staticmethod
-    def ext_path(path):
-        global chrome_extension
-        chrome_extension = path
+    def ext_path(s, path):
+        s.chrome_extension = path
 
-    @staticmethod
-    def quit_chrome():
-        if chrome_on:
-            driver.quit()
+    def quit_chrome(s):
+        if s.chrome_on:
+            s.driver.quit()
+
+    def start_phantomjs(s):
+        s.phantom_driver = webdriver.PhantomJS(s.phantom_path)
+        s.phantom_on = True
+
+    def pahntom_path(s, path):
+        s.phantom_path = path
+
